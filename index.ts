adf341e88d2f96b9bd3fa48ae75bdd1efcb82c41
Syed Saud Ali <160472981+SyedSaudAli@users.noreply.github.com>
2024-03-23 14:20:41 +0500

    Create number guessing game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const ramdonNumber = Math.floor(Math.random() * 6 + 1)
+
+const answers = await inquirer.prompt([
+  {
+    name: "UserGuessNumber",
+    type: "number",
+    message: "Please enter you guess number between 1 to 6:",
+  },
+]);
+
+if (answers.UserGuessNumber === ramdonNumber) {
+  console.log("Congratulations! you`r right guessed number");
+} else {
+  console.log("Sorry you`r wrong gussed number");
+}
